ed08951df58abf413de9a3c1872cfd74a0c8a0b6
shalu2022 <frontendstuff2022@gmail.com>
2024-08-23 01:10:31 +0530

    #11: added contact us page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import style from './contact.module.css'
+import Image from 'next/image'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className={style.container}>
+      <h1 className={style.mainHeading}>Contact Us</h1>
+      <div className={style.innerContainer}>
+        <div className={style.imageContainer}>
+          <Image className={style.img} width={500} height={400} src="/contact_us.png"/>
+        </div>
+        <div className={style.form}>
+          <input className={style.input} type='text' placeholder='First Name'/>
+          <input className={style.input} type='text' placeholder='Last Name'/>
+          <input className={style.input} type='text' placeholder='Enter email'/>
+          <input className={style.input} type='text' placeholder='Enter Phone No.'/>
+          <textarea className={style.textarea} placeholder='Message...' cols={29} rows={6}/>
+          <button type='submit' className={style.button}>Send Message</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
