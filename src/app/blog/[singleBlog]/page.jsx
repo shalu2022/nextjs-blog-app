a83853ac1a299651ce28eb2ebecf45bba0702d2d
shalu2022 <frontendstuff2022@gmail.com>
2024-08-29 00:04:48 +0530

    #6: Created blog UI

--- src/app/blog/[singleBlog]/page.jsx
+++ src/app/blog/[singleBlog]/page.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import style from './page.module.css'
+import Image from 'next/image'
 
 function SingleBlog() {
   return (
-    <div>SingleBlog</div>
+    <div className={style.container}>
+      <div className={style.upperDiv}>
+        <Image className={style.image} src='/dummy-image.png' width={500} height={300} />
+      </div>
+      <div className={style.bottomDiv}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda commodi corrupti numquam nisi excepturi omnis fugit corporis accusantium at neque, aspernatur et, dignissimos voluptatem minima. Debitis adipisci repellendus sit laborum!
+        Id minima commodi et veritatis quidem quasi vero sequi voluptas officiis hic tempora est quisquam maiores, qui incidunt similique modi provident culpa repellendus illo ut debitis corrupti ex. Temporibus, odit.
+        Suscipit eveniet vel ratione vero temporibus maxime tenetur accusamus deleniti earum nulla explicabo labore harum dolores fugiat recusandae fuga velit ipsum, corrupti molestias. Libero sit sapiente nemo ea voluptatem id?
+        Quam vero quidem iusto veniam, eveniet sint doloremque quisquam aut quos doloribus natus labore dignissimos possimus id omnis! Architecto placeat nostrum omnis laudantium qui quod sapiente laboriosam id fugit dignissimos!
+        Dolores nesciunt eaque dolorem consequatur recusandae, repellat saepe consequuntur aut repellendus rerum, magnam tempore, fuga est qui eligendi impedit. Quas quod dolores temporibus dolor facere recusandae molestias, a accusamus doloremque.
+        Cumque consequatur molestiae perspiciatis, consequuntur excepturi aliquam nesciunt quia eveniet cupiditate sapiente molestias illo eaque! Laudantium totam similique perferendis qui, cum quas non asperiores iusto dolorum voluptatum voluptatem rerum? Dolor.
+        Veniam dicta fugiat, mollitia tempore assumenda adipisci! Quod, earum debitis voluptatem dolor, unde dicta obcaecati illum rerum hic sunt ratione eaque itaque aspernatur excepturi? Obcaecati assumenda tempore ea reiciendis voluptates.
+        Cupiditate blanditiis perspiciatis reprehenderit enim numquam possimus deleniti asperiores nam pariatur eos quod harum praesentium sunt ducimus esse ipsam, ipsum ratione laboriosam, quam quis omnis nemo maxime. Sit, magnam velit!
+        </p><p>
+        Saepe voluptas repellat fuga, minus voluptatem maxime repudiandae omnis harum dolorem hic deserunt sunt sequi accusantium nulla dolor sint cumque at eum distinctio obcaecati quas earum accusamus? Excepturi, ad deleniti!
+        Officiis sit deserunt animi cupiditate ipsam fugit, voluptas consectetur odio, et, quasi ad voluptates perspiciatis quia nulla repudiandae quo. Quam explicabo accusantium alias rem magnam harum natus dolorum necessitatibus. Cumque!
+        Architecto saepe cupiditate dolorem earum labore sit maiores. Molestiae assumenda commodi veritatis velit, totam explicabo repudiandae ad voluptatum quis esse quas vitae voluptate consequatur ab tempore provident, eum libero autem?
+        Sapiente amet quas optio, iusto quaerat fuga consequuntur sint officia exercitationem similique expedita provident voluptatem nisi libero necessitatibus, earum harum facilis error dolor. Cumque eius at facilis illo. Fugit, possimus?
+        Minus, culpa expedita. Eligendi, ullam architecto commodi tempora consequuntur incidunt consectetur. Culpa quos sapiente ipsa ducimus qui voluptatum illum eaque facilis, quibusdam maiores cum rerum iste provident, numquam fuga quidem!
+        </p><p>
+        Inventore minima impedit sint aliquam. Ea et delectus exercitationem fugiat quaerat tempora maiores obcaecati laboriosam, eligendi enim dolorum, natus totam, quidem eius autem amet ab consectetur blanditiis? Voluptatibus, a cupiditate.
+        Minus esse assumenda officia ad soluta vero, eaque odit ab dolorem laudantium dolores! Praesentium tempora nesciunt nobis numquam, aut corrupti accusantium, quaerat quos, iste eos ipsum cumque mollitia non dolor?
+        Consequuntur voluptatum exercitationem, esse molestiae minima maxime asperiores voluptas non temporibus consequatur laborum autem libero, a ad ea est iste, architecto dolorem quod! Totam, magnam minus doloribus suscipit corrupti veniam.
+        Animi inventore eum impedit nisi eius consectetur fuga illum delectus sed non dolores quia reiciendis mollitia architecto, est aliquid distinctio voluptatum velit! Fuga voluptatem debitis deserunt repellendus illum maiores at.
+        </p>
+        <p>
+        Eos pariatur vero tempore quam dolor, velit repellat doloribus aperiam? Temporibus, dolor quisquam error recusandae iusto quidem ipsam unde vitae quam obcaecati hic sunt? Iure, nihil doloribus? Voluptate, quia harum.
+        Laudantium ad molestiae magnam aperiam accusantium, voluptatibus modi iusto illo omnis sapiente iure quod necessitatibus non eaque vel tempora nobis quaerat id fuga cupiditate inventore exercitationem rem? Dolorem, earum ea.
+        Nulla vel itaque possimus temporibus quo maiores praesentium ea, culpa repellendus veritatis dolore dolorem quia numquam nemo repudiandae nihil et architecto illo vitae maxime quidem quibusdam. Earum quia dolorem non.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos similique quibusdam natus quasi id quia cumque exercitationem, iure, culpa laborum iste vero ad expedita, debitis sint. Temporibus, culpa! Dolores, iusto?
+        Soluta, ex totam harum quos ducimus quidem illo. In aut debitis ea praesentium quos maxime cumque explicabo rem, dicta, itaque expedita quibusdam tempore culpa, laboriosam voluptatem earum esse architecto autem.
+        Aut reiciendis asperiores dignissimos dolor soluta sunt obcaecati, assumenda eius iste earum at nihil ducimus aperiam perferendis ratione fugit quod voluptates a aliquid nesciunt eos, officia placeat. Reiciendis, natus et.
+        Iure laborum unde repellendus ab nemo laboriosam, ad illum aliquid explicabo dignissimos nobis, doloribus ipsam est voluptate neque! Ad qui tenetur ipsum ullam architecto at debitis sit consequuntur. Nostrum, illum!
+        Accusantium, eligendi numquam nihil repudiandae enim illo, ullam officia distinctio veniam, voluptatum aperiam laborum alias in dolorum. Deserunt suscipit tempora, fugit a molestias, laborum magni quibusdam dolorem saepe, laudantium dolores!</p>
+      </div>
+    </div>
   )
 }
 
